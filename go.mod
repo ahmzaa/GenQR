65e1d4ecc4bd6a79e810cda03453f55a97400fae
ahmza <hamzaahmed12328@gmail.com>
2024-07-20 20:15:40 +0100

    taking simple aproach
    taking a more simple aproach that works. Until I can figure out bubble
    tea

--- go.mod
+++ go.mod
@@ -1,28 +1,27 @@
-module gitlab.com/ahmza/qr-code-gen
+module gitlab.com/ahmza/GenQR
 
-go 1.22.3
+go 1.22.5
+
+require (
+	github.com/charmbracelet/lipgloss v0.12.1
+	github.com/yeqown/go-qrcode/v2 v2.2.4
+	github.com/yeqown/go-qrcode/writer/standard v1.2.4
+	github.com/yeqown/go-qrcode/writer/terminal v1.1.1
+)
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/charmbracelet/bubbles v0.18.0 // indirect
-	github.com/charmbracelet/bubbletea v0.26.6 // indirect
-	github.com/charmbracelet/lipgloss v0.11.0 // indirect
-	github.com/charmbracelet/x/ansi v0.1.2 // indirect
-	github.com/charmbracelet/x/input v0.1.0 // indirect
-	github.com/charmbracelet/x/term v0.1.1 // indirect
-	github.com/charmbracelet/x/windows v0.1.0 // indirect
-	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/charmbracelet/x/ansi v0.1.4 // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
-	github.com/muesli/cancelreader v0.2.2 // indirect
-	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	golang.org/x/sync v0.7.0 // indirect
+	github.com/yeqown/reedsolomon v1.0.0 // indirect
+	golang.org/x/image v0.10.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.3.8 // indirect
 )
--- main.go
+++ main.go
@@ -1,156 +1,68 @@
 package main
 
 import (
-    "os"
     "fmt"
-//    "time"
-//    "net/http"
 
-    "github.com/charmbracelet/bubbles/spinner"
-    tea "github.com/charmbracelet/bubbletea"
+    "github.com/yeqown/go-qrcode/v2"
+    "github.com/yeqown/go-qrcode/writer/standard"
+    "github.com/yeqown/go-qrcode/writer/terminal"
+
     "github.com/charmbracelet/lipgloss"
 )
 
-var (
-    // Available Spinners
-    spinners = []spinner.Spinner{
-        spinner.Line,
-        spinner.Dot,
-        spinner.MiniDot,
-        spinner.Jump,
-        spinner.Pulse,
-        spinner.Points,
-        spinner.Globe,
-        spinner.Moon,
-        spinner.Monkey,
-    }
+var head = lipgloss.NewStyle().
+    Bold(true).
+    Foreground(lipgloss.Color("#FAFAFA")).
+    Background(lipgloss.Color("#7D56F4")).
+    PaddingTop(1).
+    PaddingRight(1).
+    PaddingBottom(1).
+    PaddingLeft(1).
+    Width(80).
+    Align(lipgloss.Center)
 
-    textStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("255")).Render
-    spinnerStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("69"))
-    helpStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Render
-)
+var callToAct = lipgloss.NewStyle().
+    Foreground(lipgloss.Color("#00FF00")).
+    Blink(true)
 
-type model struct {
-    choices  []string
-    cursor   int
-    selected map[int]struct{}
-    index    int
-    spinner  spinner.Model
-}
 
-func initialModel() model {
-    return model{
-        // The to-do list, i.e list of tasks available to the user
-        choices: []string{"Create QR Code"},
+func GenQRTerm(data string) {
+    qrc, _ := qrcode.New(data)
 
-        // The map indicates which choices are selected.
-        selected: make(map[int]struct{}),
+    w := terminal.New()
+
+    if err := qrc.Save(w); err != nil {
+        panic(err)
     }
 }
 
-func (m model) Init() tea.Cmd {
-    return m.spinner.Tick
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-    switch msg := msg.(type) {
-    // Is it a key press ?
-    case tea.KeyMsg:
-        switch msg.String() {
-        // Keys to exit the program
-        case "ctrl+c", "q":
-            return m, tea.Quit
-        // keys to move cursor up
-        case "up", "k":
-            if m.cursor > 0 {
-                m.cursor--
-            }
-        // Keys to move cursor down
-        case "down", "j":
-            if m.cursor < len(m.choices)-1 {
-                m.cursor++
-            }
-        case "left", "h":
-            m.index--
-            if m.index < 0 {
-                m.index = len(spinners) - 1
-            }
-            m.resetSpinner()
-            return m, m.spinner.Tick
-        case "right", "l":
-            m.index++
-            if m.index >= len(spinners) {
-                m.index = 0
-            }
-            m.resetSpinner()
-            return m, m.spinner.Tick
-        // Keys to toggle the selected item / state
-        case "enter", " ":
-            _, ok := m.selected[m.cursor]
-            if ok {
-                delete(m.selected, m.cursor)
-            } else {
-                m.selected[m.cursor] = struct{}{}
-            }
-        }
+func GenQRFile(data string, path string) {
+    qrc, err := qrcode.New(data)
+    if err != nil {
+        fmt.Printf("could not generate QRCode: %v", err)
+        return
     }
 
-    // return the updated model to Bubble tea runtime
-    // Note: no commands being returned.
-    return m, nil
-}
-
-func (m *model) resetSpinner() {
-    m.spinner = spinner.New()
-    m.spinner.Style = spinnerStyle
-    m.spinner.Spinner = spinners[m.index]
-}
-
-func (m model) View() string {
-    // Header
-    s := "QR Code Generator\n\n"
-
-    // Iterate over choices
-    for i, choice := range m.choices {
-        
-        // Is cursor pointing at this choice ?
-        cursor := " " // no cursor
-        if m.cursor == i {
-            cursor = ">" // cursor
-        }
-
-        // Is this choice selected?
-        checked := " " // Note selected
-        if _, ok := m.selected[i]; ok {
-            checked = "x" // selected
-        }
-
-        //render the row
-        s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+    w, err := standard.New(path)
+    if err != nil {
+        fmt.Printf("standard.New failed: %v", err)
+        return
     }
 
-    var gap string
-    switch m.index {
-    case 1:
-        gap = ""
-    default:
-        gap = " "
+    // save file
+    if err = qrc.Save(w); err != nil {
+        fmt.Printf("could not save image: %v", err)
     }
-
-    s += fmt.Sprintf("\n %s%s%s\n\n", m.spinner.View(), gap, textStyle("Spinning..."))
-    s += helpStyle("h/l, ←/→: change spinner • q: exit\n")
-
-    // Footer
-    s += "\nwritten by ahmza\n"
-    s += "\nPress q to quit\n"
-
-    return s
 }
 
 func main() {
-    p := tea.NewProgram(initialModel())
-    if _, err := p.Run(); err != nil {
-        fmt.Printf("LoL, there's been an error: %v", err)
-        os.Exit(1)
-    }
+    fmt.Println(head.Render("GenQR - Your Terminal QR code Generator"))
+    fmt.Println(callToAct.Render("Enter the data you would like to encode: "))
+    var data string
+    fmt.Scanln(&data)
+    GenQRTerm(data)
+    fmt.Println(callToAct.Render("Enter the path & filename you would like to save to: "))
+    var path string
+    fmt.Scanln(&path)
+    GenQRFile(data, path)
 }
