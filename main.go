1369403b13ed1f52603eb0244d45c4ab644497b9
Hamza Ahmed <hamzaahmed12328@gmail.com>
2024-07-25 08:18:13 +0100

    Changed from fmt to bufio for user input

--- main.go
+++ main.go
@@ -1,29 +1,42 @@
 package main
 
 import (
-    "fmt"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 
-    "github.com/yeqown/go-qrcode/v2"
-    "github.com/yeqown/go-qrcode/writer/standard"
-    "github.com/yeqown/go-qrcode/writer/terminal"
+	"github.com/yeqown/go-qrcode/v2"
+	"github.com/yeqown/go-qrcode/writer/standard"
+	"github.com/yeqown/go-qrcode/writer/terminal"
 
-    "github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss"
 )
 
-var head = lipgloss.NewStyle().
+var headStyle = lipgloss.NewStyle().
     Bold(true).
-    Foreground(lipgloss.Color("#FAFAFA")).
-    Background(lipgloss.Color("#7D56F4")).
+    Foreground(lipgloss.Color("#585B70")).
+    Background(lipgloss.Color("#A6E3A1")).
     PaddingTop(1).
-    PaddingRight(1).
+    //PaddingRight(1).
     PaddingBottom(1).
-    PaddingLeft(1).
+    //PaddingLeft(1).
     Width(80).
     Align(lipgloss.Center)
 
-var callToAct = lipgloss.NewStyle().
+var callToActStyle = lipgloss.NewStyle().
     Foreground(lipgloss.Color("#00FF00")).
     Blink(true)
+
+var helpStyle = lipgloss.NewStyle().
+    Foreground(lipgloss.Color("#A6ADC8")).
+    Background(lipgloss.Color("#45475A")).
+    PaddingTop(1).
+    //PaddingRight(1).
+    PaddingBottom(1).
+    //PaddingLeft(1).
+    Width(80).
+    Align(lipgloss.Center)
 
 
 func GenQRTerm(data string) {
@@ -56,13 +69,26 @@ func GenQRFile(data string, path string) {
 }
 
 func main() {
-    fmt.Println(head.Render("GenQR - Your Terminal QR code Generator"))
-    fmt.Println(callToAct.Render("Enter the data you would like to encode: "))
-    var data string
-    fmt.Scanln(&data)
+    scanner := bufio.NewReader(os.Stdin)
+
+    fmt.Println(headStyle.Render("GenQR - Your Terminal QR code Generator"))
+    fmt.Println(helpStyle.Render("HELP\n\nQuit: Ctrl+C      Enter: Enter"))
+
+    fmt.Println(callToActStyle.Render("Enter the data you would like to encode: "))
+    data, err := scanner.ReadString('\n')
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    data = strings.TrimSpace(data)
     GenQRTerm(data)
-    fmt.Println(callToAct.Render("Enter the path & filename you would like to save to: "))
-    var path string
-    fmt.Scanln(&path)
+
+    fmt.Println(callToActStyle.Render("Enter the path & filename you would like to save to: "))
+    path, err := scanner.ReadString('\n')
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    path = strings.TrimSpace(path)
     GenQRFile(data, path)
 }
