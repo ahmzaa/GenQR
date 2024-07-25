d94c934d7b5673bde0542bcf8d722e6ecf165f54
ahmza <hamzaahmed12328@gmail.com>
2024-07-25 20:00:18 +0100

    refactored
    - more modular
    - cleaner
    - more error handling

--- main.go
+++ main.go
@@ -13,83 +13,95 @@ import (
     "github.com/charmbracelet/lipgloss"
 )
 
-// Style used for header
-var headStyle = lipgloss.NewStyle().
-    Bold(true).
-    Foreground(lipgloss.Color("#585B70")).
-    Background(lipgloss.Color("#A6E3A1")).
-    PaddingTop(1).
-    PaddingBottom(1).
-    Width(80).
-    Align(lipgloss.Center)
+// Styles for different parts of the application
+var (
+    headStyle = lipgloss.NewStyle().
+        Bold(true).
+        Foreground(lipgloss.Color("#585B70")).
+        Background(lipgloss.Color("#A6E3A1")).
+        PaddingTop(1).
+        PaddingBottom(1).
+        Width(80).
+        Align(lipgloss.Center)
 
-// Style used when user asked for input
-var callToActStyle = lipgloss.NewStyle().
-    Foreground(lipgloss.Color("#00FF00")).
-    Blink(true)
+    helpStyle = lipgloss.NewStyle().
+        Foreground(lipgloss.Color("#A6ADC8")).
+        Background(lipgloss.Color("#45475A")).
+        PaddingTop(1).
+        PaddingBottom(1).
+        Width(80).
+        Align(lipgloss.Center)
 
-// For the help bar
-var helpStyle = lipgloss.NewStyle().
-    Foreground(lipgloss.Color("#A6ADC8")).
-    Background(lipgloss.Color("#45475A")).
-    PaddingTop(1).
-    PaddingBottom(1).
-    Width(80).
-    Align(lipgloss.Center)
+    callToActStyle = lipgloss.NewStyle().
+        Foreground(lipgloss.Color("#00FF00")).
+        Blink(true)
+)
 
-
-// Generates the QRCode and outputs to stdout (Visual in the terminal)
-func GenQRTerm(data string) {
-    qrc, _ := qrcode.New(data)
-
-    w := terminal.New()
-
-    if err := qrc.Save(w); err != nil {
-        panic(err)
-    }
-}
-
-// Export the generated QRCode to an image file
-func GenQRFile(data string, path string) {
+// GenQRTerm generates the QRCode and outputs to stdout (visual in the terminal)
+func GenQRTerm(data string) error {
     qrc, err := qrcode.New(data)
     if err != nil {
-        fmt.Printf("could not generate QRCode: %v", err)
-        return
+        return fmt.Errorf("could not generate QRCode: %v", err)
+    }
+
+    w := terminal.New()
+    if err := qrc.Save(w); err != nil {
+        return fmt.Errorf("could not output QRCode to terminal: %v", err)
+    }
+    return nil
+}
+
+// GenQRFile exports the generated QRCode to an image file
+func GenQRFile(data, path string) error {
+    qrc, err := qrcode.New(data)
+    if err != nil {
+        return fmt.Errorf("could not generate QRCode: %v", err)
     }
 
     w, err := standard.New(path)
     if err != nil {
-        fmt.Printf("standard.New failed: %v", err)
-        return
+        return fmt.Errorf("failed to create the writer: %v", err)
     }
 
-    // save file
     if err = qrc.Save(w); err != nil {
-        fmt.Printf("could not save image: %v", err)
+        return fmt.Errorf("could not save image: %v", err)
     }
+    return nil
+}
+
+// promptUser prompts the user for input and return the input string
+func promptUser(promptStyle lipgloss.Style, promptText string) (string, error) {
+    scanner := bufio.NewReader(os.Stdin)
+    fmt.Println(promptStyle.Render(promptText))
+    input, err := scanner.ReadString('\n')
+    if err != nil {
+        return "", fmt.Errorf("error reading input: %v", err)
+    }
+    return strings.TrimSpace(input), nil
 }
 
 func main() {
-    scanner := bufio.NewReader(os.Stdin)
-
     fmt.Println(headStyle.Render("GenQR - Your Terminal QR code Generator"))
     fmt.Println(helpStyle.Render("HELP\n\nQuit: Ctrl+C      Enter: Enter"))
 
-    fmt.Println(callToActStyle.Render("Enter the data you would like to encode: "))
-    data, err := scanner.ReadString('\n')
+    data, err := promptUser(callToActStyle, "Enter data to encode:")
     if err != nil {
         fmt.Println(err)
         return
     }
-    data = strings.TrimSpace(data)
-    GenQRTerm(data)
 
-    fmt.Println(callToActStyle.Render("Enter the path & filename you would like to save to: "))
-    path, err := scanner.ReadString('\n')
+    if err := GenQRTerm(data); err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    path, err := promptUser(callToActStyle, "Enter the path & filename to save:")
     if err != nil {
         fmt.Println(err)
         return
     }
-    path = strings.TrimSpace(path)
-    GenQRFile(data, path)
+
+    if err := GenQRFile(data, path); err != nil {
+        fmt.Println(err)
+    }
 }
